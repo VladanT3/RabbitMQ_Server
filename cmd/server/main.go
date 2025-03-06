324d2ae84f981503d773793a4fdab043bbd3bb56
VladanT3 <vladt.t33@gmail.com>
2025-03-06 14:54:26 +0100

    added repl command functionality to the server side

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -27,7 +28,32 @@ func main() {
 		log.Fatal("Couldn't create channel: ", err)
 	}
 
-	err = pubsub.PublishJSON(channel, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	gamelogic.PrintServerHelp()
+
+	for {
+		input := gamelogic.GetInput()
+		if input == nil || len(input) == 0 {
+			continue
+		}
+		if input[0] == "pause" {
+			fmt.Println("Pausing the game...")
+			err = pubsub.PublishJSON(channel, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Fatal("Error sending 'pause' message: ", err)
+			}
+		} else if input[0] == "resume" {
+			fmt.Println("Resuming the game...")
+			err = pubsub.PublishJSON(channel, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Fatal("Error sending 'resume' message: ", err)
+			}
+		} else if input[0] == "quit" {
+			fmt.Println("Quiting the game...")
+			break
+		} else {
+			fmt.Println("Unknown command.")
+		}
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
